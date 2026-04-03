59d340cfb365a6acfd927af3a5eb913ab4b842ea
peterhuangmchp-cloud <peter.huangmchp@gmail.com>
2026-04-03 13:07:46 +0800

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -34,7 +34,7 @@ gsheet_id = st.secrets.get("GSHEET_ID")
 @st.cache_data(ttl=600) # 縮短快取時間，方便即時更新
 def load_data(sheet_id):
     # 修正 gid 連結與空格問題
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=0"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=601349851"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
     data = pd.read_csv(io.StringIO(response.text))
